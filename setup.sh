7dad54b823cd746a130b76d52a56f3de96ff7478
Daniel Golle <daniel.golle@gmail.com>
2011-02-05 16:51:03 +0000

    Refactored things in order to get a more reproducable way to generate a firmware image.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+svn co -r 23429 svn://svn.openwrt.org/openwrt/branches/backfire openwrt || exit 1
+
+cd openwrt
+[ ! -e .config ] && cp -v ../config .config
+[ ! -e feeds.conf ] && cp -v feeds.conf.default feeds.conf
+if ! grep -q commotion feeds.conf; then
+    echo "adding commotion package feed..."
+    echo "src-link commotion ../../commotionfeed" >> feeds.conf
+fi
+
+scripts/feeds update
+scripts/feeds install commotionbase
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo " Commotion OpenWrt is prepared. To build the firmware, type:"
+echo " cd openwrt"
+echo " make menuconfig"
+echo " make V=99"
