00f80e59e0272a648097666290ff753334e7afde
Josh King <josh@chambana.net>
2012-01-10 10:25:29 -0500

    Minor build process fixes, pinning checkout to Backfire 10.03.1 tag for now.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-svn co -r 28680 svn://svn.openwrt.org/openwrt/branches/backfire openwrt || exit 1
+svn co svn://svn.openwrt.org/openwrt/tags/backfire_10.03.1 openwrt || exit 1
 
 cd openwrt
 [ ! -e .config ] && cp -v ../config .config
+[ ! -e .config.old ] && cp -v ../config .config.old
 [ ! -e feeds.conf ] && cp -v feeds.conf.default feeds.conf
 [ ! -e files ] && mkdir files
+[ ! -e dl ] && mkdir ../dl && ln -sf ../dl dl
 cp -rf -v ../default-files/* files/
 if ! grep -q commotion feeds.conf; then
     echo "adding commotion package feed..."
@@ -13,10 +15,11 @@ if ! grep -q commotion feeds.conf; then
 fi
 
 scripts/feeds update -a
+scripts/feeds install -a
 for i in $(ls ../commotionfeed/); do scripts/feeds install $i; done
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo " Commotion OpenWrt is prepared. To build the firmware, type:"
 echo " cd openwrt"
-echo " make menuconfig"
+echo " make menuconfig #If you wish to add or change packages."
 echo " make V=99"
