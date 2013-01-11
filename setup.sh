57314b719576d3218598f9b47c4ac7eacc7c4aae
ben <ben@CampDellBuntu.(none)>
2013-01-10 20:37:04 -0600

    adding ath9k patches

--- setup.sh
+++ setup.sh
@@ -17,7 +17,13 @@ scripts/feeds update -a
 scripts/feeds install -a
 for i in $(ls ../commotionfeed/); do scripts/feeds install $i; done
 
+# Copy in Commotion-specific patches
 cp -v ../patches/910-fix-out-of-bounds-index.patch feeds/packages/utils/collectd/patches/
+cp -v ../patches/890_ath9k_advertize_beacon_int_infra_match.patch package/mac80211/patches
+cp -v ../patches/891_ath9k_htc_advertize_allowed_vif_combinations.patch package/mac80211/patches
+cp -v ../patches/892_ath9k_htc_remove_interface_combination_specific_checks.patch package/mac80211/patches
+cp -v ../patches/893_mac80211_add_supported_rates_change_notification_ibss.patch package/mac80211/patches
+cp -v ../patches/894_ath9k_htc_implement_sta_rc_update_mac80211_callback.patch package/mac80211/patches
 cp -v ../config .config
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
