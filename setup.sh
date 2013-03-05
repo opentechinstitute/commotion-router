20bc1557e5f542ee77a48d6d0208519c22f8d1f1
Dan Staples <dan.staples@riseup.net>
2013-03-05 11:38:01 -0500

    changed default config

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ scripts/feeds install -p commotion olsrd libldns
 # Copy in Commotion-specific patches
 #cp -v ../patches/910-fix-out-of-bounds-index.patch feeds/packages/utils/collectd/patches/
 #cp -v ../config .config
-cp -v ../config-aa-dr1-modules .config
+cp -v ../config-known-good .config
 
 # Remove outdated patch
 #    echo "Removing outdated AR933X_WMAC_reset_code patch ..."
