ed9b0d2760b0110a52164c12891e41061857dbf4
Andrew Reynolds <andrew@opentechinstitute.org>
2013-03-11 10:53:14 -0400

    Collectd patch 910 still needed

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ scripts/feeds install -p commotion olsrd libldns
 #for i in $(ls ../commotionfeed/); do scripts/feeds install $i; done
 
 # Copy in Commotion-specific patches
-#cp -v ../patches/910-fix-out-of-bounds-index.patch feeds/packages/utils/collectd/patches/
+cp -v ../patches/910-fix-out-of-bounds-index.patch feeds/packages/utils/collectd/patches/
 #cp -v ../config .config
 cp -v ../config-known-good .config
 
