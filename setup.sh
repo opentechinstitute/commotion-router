aaa2ab8e155167f8250e894ee1b6eee0cc496694
Dan Staples <danstaples@opentechinstitute.org>
2013-04-17 18:36:38 +0200

    added patch for AR71XX Makefile so image can be flashed via AirOS web interface

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-#svn co -r 31639 svn://svn.openwrt.org/openwrt/trunk openwrt || exit 1
 svn co svn://svn.openwrt.org/openwrt/branches/attitude_adjustment openwrt || exit 1
 
 cd openwrt
 
 [ ! -e feeds.conf ] && cp -v ../feeds.conf feeds.conf
 [ ! -e files ] && mkdir files
-#[ ! -e dl ] && mkdir ../dl && ln -sf ../dl dl
+
 cp -rf -v ../default-files/* files/
 if ! grep -q commotion feeds.conf; then
     echo "adding commotion package feed..."
@@ -18,27 +17,13 @@ scripts/feeds update -a
 scripts/feeds install -a
 scripts/feeds uninstall olsrd libldns
 scripts/feeds install -p commotion olsrd libldns
-#for i in $(ls ../commotionfeed/); do scripts/feeds install $i; done
 
 # Copy in Commotion-specific patches
 cp -v ../patches/910-fix-out-of-bounds-index.patch feeds/packages/utils/collectd/patches/
 cp -v ../config .config
 
-# Remove outdated patch
-#    echo "Removing outdated AR933X_WMAC_reset_code patch ..."
-#patch -p0 < ../patches/delete_AR933X_WMAC_reset_code.patch
-#
-## Backport compat-wireless-2012-09-07
-#if ! grep -q 2012-09-07 package/mac80211/Makefile; then
-#    echo "backporting compat-wireless-2012-09-07 drivers..."
-#    patch -p0 < ../patches/backport_compat_wireless_09-07-12.patch
-#fi
-#
-## Backport kernel 3.3.8
-#if ! grep -q 3.3.8 include/kernel-version.mk; then
-#    echo "backporting kernel v3.3.8..."
-#    patch -p0 < ../patches/backport_kernel_3.3.8.patch
-#fi
+# Patch AR71XX makefile so image can be flashed through the AirOS web interface
+sed -i s/'$(eval $(call SingleProfile,UBNTXM,$(fs_64k),UBNTUNIFI,ubnt-unifi,UBNT-UF,ttyS0,115200,XM,XM,ar7240))'/'$(eval $(call SingleProfile,UBNTXM,$(fs_64k),UBNTUNIFI,ubnt-unifi,UBNT-UF,ttyS0,115200,XM,UBNTXM,ar7240))'/ openwrt/target/linux/ar71xx/image/Makefile
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo " Commotion OpenWrt is prepared. To build the firmware, type:"
