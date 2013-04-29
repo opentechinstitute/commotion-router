20d93e6f7e98b6b65d789fb45bf472c98579055a
Dan Staples <dan.staples@riseup.net>
2013-04-29 09:36:03 -0400

    switched openwrt to git clone the now-stable AA branch

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-#svn co -r 31639 svn://svn.openwrt.org/openwrt/trunk openwrt || exit 1
-svn co svn://svn.openwrt.org/openwrt/branches/attitude_adjustment openwrt || exit 1
+git clone git://git.openwrt.org/12.09/openwrt.git
 
 cd openwrt
 
 [ ! -e feeds.conf ] && cp -v ../feeds.conf feeds.conf
 [ ! -e files ] && mkdir files
-#[ ! -e dl ] && mkdir ../dl && ln -sf ../dl dl
 cp -rf -v ../default-files/* files/
 if ! grep -q commotion feeds.conf; then
     echo "adding commotion package feed..."
@@ -18,27 +16,10 @@ scripts/feeds update -a
 scripts/feeds install -a
 scripts/feeds uninstall olsrd libldns
 scripts/feeds install -p commotion olsrd libldns
-#for i in $(ls ../commotionfeed/); do scripts/feeds install $i; done
 
 # Copy in Commotion-specific patches
 cp -v ../patches/910-fix-out-of-bounds-index.patch feeds/packages/utils/collectd/patches/
 cp -v ../config .config
-
-# Remove outdated patch
-#    echo "Removing outdated AR933X_WMAC_reset_code patch ..."
-#patch -p0 < ../patches/delete_AR933X_WMAC_reset_code.patch
-#
-## Backport compat-wireless-2012-09-07
-#if ! grep -q 2012-09-07 package/mac80211/Makefile; then
-#    echo "backporting compat-wireless-2012-09-07 drivers..."
-#    patch -p0 < ../patches/backport_compat_wireless_09-07-12.patch
-#fi
-#
-## Backport kernel 3.3.8
-#if ! grep -q 3.3.8 include/kernel-version.mk; then
-#    echo "backporting kernel v3.3.8..."
-#    patch -p0 < ../patches/backport_kernel_3.3.8.patch
-#fi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo " Commotion OpenWrt is prepared. To build the firmware, type:"
@@ -47,4 +28,3 @@ echo " make menuconfig #If you wish to add or change packages."
 echo " make V=99"
 echo " Please make use of the DR1rc wiki page:"
 echo " https://code.commotionwireless.net/projects/commotion/wiki/AttitudeAdjustmentBuild"
-
